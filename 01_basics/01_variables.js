dd76713eab29fac00f518fda14fb47615519e89c
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-01 18:22:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345   // no change coz constant
+let accountEmail = "test@gmail.com"
+var  accountPassword = "123123"  // not used because of scope problem
+accountCity = "Jaipur"  // very bad practice
+let accountState;
+
+// accountId = 2  // not allowed
+
+accountEmail = "hc@gh.com"
+accountPassword = "1234567"
+accountCity = "delhi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and finctional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
