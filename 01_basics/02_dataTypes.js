af84d15a24037985d8f85caacfddb5ded8d0bd1b
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-01 18:39:39 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using node.js, not browser
+
+// code readability should be high
+
+// documentation = mdn, tc39.es
+
+// console.log(3 + 3)
+
+// console.log("Priya")
+
+let name = "priya" // string
+let age = 18 // number
+let isLoggedIn = false // bool
+
+
+// number => 2 to power 53 (range)
+// bigint (for very big number)
+// string (use in eithr single or double quotes)
+// boolean => true/false
+// null (datatype as well as a standalone value) (representation of empty value)
+// undefined => value not assigned yet
+// symbol => used for uniqueness
+
+
+// object
+
+console.log(typeof "priya");
+console.log(typeof 18);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
