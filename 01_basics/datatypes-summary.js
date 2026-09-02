194e8081a296b038111ea493c23fb0dd28e37eda
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-02 16:08:22 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,53 @@
+// Primitive (call by value(copy of data))
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Is javascript statically typed or dynamically typed?
+/* JavaScript is a dynamically typed language, which means that data types of 
+variables are determined by the value they hold at runtime and can change
+ throughout the program as we assign different values to them.*/
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 123456789012345678n // bigInt
+
+// Reference (Non primitive)(call by reference)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Priya",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+/* Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object */
