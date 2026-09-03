6c9e12a699fed608ccadb11704fdcec73bb6145f
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-03 04:19:44 +0000

    Compare

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -13,13 +13,13 @@ console.log("02" > 1);  // true
 
 console.log(null > 0);  // false
 console.log(null == 0);  // false  // works differently
-console.log(null >= 0);  // true  //conversion happens ..null converted to 0
+console.log(null >= 0);  // true  // conversion happens ..null converted to 0
 (the reason is that an equality check == and comparisons
 > < >= <= work differently. Comparisons convert null to
 a number, treating it as 0. Thas why (3) null >=0 is true and
 (1) null > 0 is false)
 
-console.log(undefined == 0); //always false in any comparison in undefined
+console.log(undefined == 0); // always false in any comparison in undefined
 console.log(undefined > 0);
 console.log(undefined < 0);
 
@@ -27,9 +27,3 @@ console.log(undefined < 0);
 
 // === (strict check) (also checks datatype)
 console.log("2" === 2);  // false
-
-
-
-
-
-
