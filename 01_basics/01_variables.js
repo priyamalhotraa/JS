b28cae236d4fb5763125f1134f64252f3f9923aa
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-03 04:19:06 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -14,7 +14,7 @@ console.log(accountId);
 
 /*
 Prefer not to use var
-because of issue in block scope and finctional scope
+because of issue in block scope and functional scope
 */
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
