8d33d9097e2fe74ac10dec7e7fc44706ae158b7b
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-07 10:33:17 +0000

    object implementation

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+const tinderUser = new Object()  // singleton object (constructor)
+// const tinderUser = {} // non singleton object (literal)
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedin = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "priya",
+            lastname: "malhotra"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);  // accessing object nesting here
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 } // same array problem where object2 is merged inside the object1
+
+const obj3 = {...obj1, ...obj2} // spread operator
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "p@gmail.com"
+    },
+    {
+        id: 1,
+        email: "p@gmail.com"
+    },
+    {
+        id: 1,
+        email: "p@gmail.com"
+    },
+    
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));  // important  // [ 'id', 'name', 'isLoggedain' ]
+console.log(Object.values(tinderUser)); // [ '123abc', 'sammy', false ]
+console.log(Object.entries(tinderUser));  // [ [ 'id', '123abc' ], [ 'name', 'sammy' ], [ 'isLoggedain', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedin')); // checks if the user that property or not
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
