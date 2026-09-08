5162d8043b0a30cbe319dd0fdfda69c0c2bc1bdf
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-08 09:22:35 +0000

    Scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+// var c = 400
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
