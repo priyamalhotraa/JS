f02ac104d0bd52ac9b4490a0eb12353dc9afd2d8
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-10 06:56:18 +0000

    this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,9 +4,21 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);  // this refers to the current context
-        console.log(`the price is ${this.price}, please proceed to pay it`);
+        // console.log(`the price is ${this.price}, please proceed to pay it`);
+        console.log(this);
     }
 
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// in browser it shows window, because window is the most global object in the browser
+
+function chai(){
+    let username = "priya"
+    console.log(this.username);
+}
+
+chai()
