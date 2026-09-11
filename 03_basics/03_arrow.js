4b1a8461f626017a82ee57072f2852e71844484f
Priya Malhotra <priyamalhotra14012005@gmail.com>
2026-09-11 04:27:40 +0000

    arrows and this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -16,9 +16,36 @@ const user = {
 
 // in browser it shows window, because window is the most global object in the browser
 
-function chai(){
-    let username = "priya"
-    console.log(this.username);
-}
+// function chai(){
+//     let username = "priya"
+//     console.log(this.username);
+// }
 
-chai()
+// chai()
+
+// const chai = function() {
+//     let username = "priya"
+//     console.log(this.username);
+// }
+
+// const chai = () => {  // arrow function
+//     let username = "priya"
+//     console.log(this);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2  // implicit return   // no need for return 
+// const addTwo = (num1, num2) =>  (num1 + num2)  
+// if curly braces are there, then write return keyword; if parenthesis, then no return keyword
+
+const addTwo = (num1, num2) =>  ({username: "priya"})  // this is an object to be returned, and for returning object you have to warp it in parenthesis otherwise it will not work
+
+console.log(addTwo(3, 4))
+
+
+const myArray = [2, 3, 5, 7, 8]
